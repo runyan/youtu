ff738edb87b86a1154bdfc0cdd84ac800508bcfa
runyan <yanrun2007@gmail.com>
2018-09-10 22:36:08 +0800

    update

--- src/main/java/com/maoxiong/youtu/App.java
+++ src/main/java/com/maoxiong/youtu/App.java
@@ -68,10 +68,10 @@ public class App {
 	
 	private static final Logger logger = LogManager.getLogger(App.class);
 	
-	private static final String QQ = "530913397";
-	private static final String APP_ID = "10125863";
-	private static final String SECRET_ID = "AKIDnV8bFlTMRnPxMbhtoZEhzCIGyZ2K5aTB";
-	private static final String SECRET_KEY = "lLl613yk3kKxTp6LRXVJwPhonjopjT6b";
+	private static final String QQ = "";
+	private static final String APP_ID = "";
+	private static final String SECRET_ID = "";
+	private static final String SECRET_KEY = "";
 	
     public static void main( String[] args ) {
     	Initializer initializer = new Initializer.Builder()
